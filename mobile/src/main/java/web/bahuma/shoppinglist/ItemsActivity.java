f06fd2115d9e8b2c231f70ae0dac5111065667f5
bahuma20 <bahuma20@bahuma-online.de>
2014-07-05 19:00:49 +0200

    Some file renaming

--- mobile/src/main/java/web/bahuma/shoppinglist/ItemsActivity.java
+++ mobile/src/main/java/web/bahuma/shoppinglist/ItemsActivity.java
@@ -4,21 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import web.bahuma.shoppinglist.R;
 
-
-public class Main extends ActionBarActivity {
+public class ItemsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_items);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.items, menu);
         return true;
     }
 
--- mobile/src/main/java/web/bahuma/shoppinglist/ShopsActivity.java
+++ mobile/src/main/java/web/bahuma/shoppinglist/ShopsActivity.java
@@ -4,21 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import web.bahuma.shoppinglist.R;
 
-public class Items extends ActionBarActivity {
+
+public class ShopsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_items);
+        setContentView(R.layout.activity_shops);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.items, menu);
+        getMenuInflater().inflate(R.menu.shops, menu);
         return true;
     }
 
