68a388370f1a2693a48ab7701417c2842c867cf8
bahuma20 <bahuma20@bahuma-online.de>
2014-07-05 18:51:02 +0200

    Added "DatabaseManager" Class

---
+++ mobile/src/main/java/web/bahuma/shoppinglist/DatabaseManager.java
@@ -0,0 +1,40 @@
+package web.bahuma.shoppinglist;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Max on 05.07.2014.
+ */
+public class DatabaseManager extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "ShoppingList.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String CREATE_SHOPS = "CREATE TABLE shops (" +
+            "_id INTEGER PRIMARY KEY AUTOINCREMENT " +
+            "name TEXT NOT NULL" +
+            ")";
+
+    private static final String CREATE_ITEMS = "CREATE TABLE items (" +
+            "_id INTEGER PRIMARY KEY AUTOINCREMENT " +
+            "shop INTEGER NOT NULL " +
+            "name TEXT NOT NULL " +
+            "bought BOOLEAN" +
+            ")";
+
+
+    public DatabaseManager(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
