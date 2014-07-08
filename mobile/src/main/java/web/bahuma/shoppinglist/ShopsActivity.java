f76bfc3d9639615575323d538c3edc69e269a6cb
bahuma20 <bahuma20@bahuma-online.de>
2014-07-08 20:30:47 +0200

    Added ability to add items

--- mobile/src/main/java/web/bahuma/shoppinglist/ShopsActivity.java
+++ mobile/src/main/java/web/bahuma/shoppinglist/ShopsActivity.java
@@ -1,12 +1,16 @@
 package web.bahuma.shoppinglist;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.renderscript.Sampler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
@@ -15,6 +19,7 @@ import android.widget.Toast;
 public class ShopsActivity extends ActionBarActivity {
     private DatabaseManager mHelper;
     private SQLiteDatabase mDatabase;
+    private AlertDialog.Builder mBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +64,22 @@ public class ShopsActivity extends ActionBarActivity {
         if (id == R.id.action_add_shop) {
             Toast.makeText(this, "Add Shop clicked", Toast.LENGTH_SHORT).show();
 
+            final EditText input = new EditText(this);
+
+            mBuilder = new AlertDialog.Builder(this);
+            mBuilder.setTitle(R.string.add_shops_title)
+                    .setIcon(android.R.drawable.ic_input_add)
+                    .setMessage(R.string.add_shops_message)
+                    .setView(input)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            addShop(input.getText().toString());
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .show();
+
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -82,5 +103,12 @@ public class ShopsActivity extends ActionBarActivity {
 
         final ListView shopsList = (ListView) findViewById(R.id.list_shops);
         shopsList.setAdapter(shopsAdapter);
+    }
+
+    private void addShop(String name) {
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        mDatabase.insert("shops", null, values);
+        loadShops();
     }
 }
