5d11b4c7dde9dd0108ad4d15ea9d6f98cdbc61f5
bahuma20 <bahuma20@bahuma-online.de>
2014-07-08 20:13:02 +0200

    Filling ListView

--- mobile/src/main/java/web/bahuma/shoppinglist/ShopsActivity.java
+++ mobile/src/main/java/web/bahuma/shoppinglist/ShopsActivity.java
@@ -1,20 +1,42 @@
 package web.bahuma.shoppinglist;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 
 public class ShopsActivity extends ActionBarActivity {
+    private DatabaseManager mHelper;
+    private SQLiteDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shops);
+
+        mHelper = new DatabaseManager(this);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDatabase = mHelper.getReadableDatabase();
+
+        loadShops();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDatabase.close();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,8 +58,29 @@ public class ShopsActivity extends ActionBarActivity {
         }
         if (id == R.id.action_add_shop) {
             Toast.makeText(this, "Add Shop clicked", Toast.LENGTH_SHORT).show();
+
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadShops() {
+        Cursor shopCursor = mDatabase.query(
+                "shops",
+                new String[] {"_id", "name"},
+                null, null, null, null,
+                "name");
+        startManagingCursor(shopCursor);
+
+        SimpleCursorAdapter shopsAdapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                shopCursor,
+                new String[] {"name"},
+                new int[] {android.R.id.text1}
+        );
+
+        final ListView shopsList = (ListView) findViewById(R.id.list_shops);
+        shopsList.setAdapter(shopsAdapter);
     }
 }
